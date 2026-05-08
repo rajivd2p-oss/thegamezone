7b04031896e74d30d8a07cbad3c64c80050ffde3
phthallo <annabelquach13@gmail.com>
2026-05-07 20:00:34 -0400

    restructure for deployment

--- utils.py
+++ utils.py
@@ -1,37 +1,38 @@
+import base64
 import hashlib
-import json 
+import json
+from cryptography.fernet import Fernet
+
+
+def _derive_key(secret: str) -> bytes:
+    return base64.urlsafe_b64encode(hashlib.sha256(secret.encode()).digest())
+
 
 def convert_to_safe_location(location_pair):
-   # locations is a zipped list of file name, location
-   location_pair = list(location_pair)
-   safe_locations = [
+    location_pair = list(location_pair)
+    return [
         {
             "device": pairing[0][:-5],
+            "label": pairing[0][:-5],
             "timestamp": pairing[1].timestamp.isoformat(),
-            "lat": pairing[1].latitude,
-            "long": pairing[1].longitude
-        } for pairing in location_pair
-   ]
-   return safe_locations
+            "latitude": pairing[1].latitude,
+            "longitude": pairing[1].longitude,
+        }
+        for pairing in location_pair
+    ]
 
 
-def encrypt_data(data, secret, output_filepath):
-    key = hashlib.sha256(secret.encode()).digest()
-
-    encrypted = bytes(b ^ key[i % len(key)] for i, b in enumerate(data.encode()))
-    
+def encrypt_data(data: str, secret: str, output_filepath: str) -> str:
+    fernet = Fernet(_derive_key(secret))
+    encrypted = fernet.encrypt(data.encode())
     with open(output_filepath, "wb") as f:
         f.write(encrypted)
-    
     return output_filepath
 
 
-def decrypt_data(filepath, secret):
-    key = hashlib.sha256(secret.encode()).digest()
-
+def decrypt_data(filepath: str, secret: str) -> dict:
+    fernet = Fernet(_derive_key(secret))
     with open(filepath, "rb") as f:
         encrypted = f.read()
-
-    decrypted = bytes(b ^ key[i % len(key)] for i, b in enumerate(encrypted))
+    decrypted = fernet.decrypt(encrypted)
     return json.loads(decrypted)
-
